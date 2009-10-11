d2e23188bd705895569c86a280b96938b2e0c11d
Yves Senn <yves.senn@gmail.com>
2009-10-11 18:10:13 +0200

    added prject lookup method

---
+++ emacs-xcode.el
@@ -0,0 +1,84 @@
+;; emacs-xcode.el --- an interface to the Eclipse IDE.
+;;
+;; Copyright (C) 2009  Tassilo Horn <tassilo@member.fsf.org>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Contributors
+;;
+;;  - Yves Senn <yves senn * gmx ch>
+;;
+;;; Conventions
+;;
+;; Conventions used in this file: Name internal variables and functions
+;; "xcode--<descriptive-name>", and name xcode command invocations
+;; "xcode/command-name", like xcode/build.
+
+;;* emacs-xcode
+
+(defvar *xcode-project-root* nil)
+
+(defun xcode--project-root ()
+  (message (or *xcode-project-root*
+      (setq *xcode-project-root* (xcode--project-lookup)))))
+
+(defun xcode--project-lookup (&optional current-directory)
+  (when (null current-directory) (setq current-directory default-directory))
+  (cond ((xcode--project-for-directory (directory-files current-directory)) (expand-file-name current-directory))
+        ((equal (expand-file-name current-directory) "/") nil)
+        (t (xcode--project-lookup (concat (file-name-as-directory current-directory) "..")))))
+
+(defun xcode--project-for-directory (files)
+  (let ((project-file nil))
+    (dolist (file files project-file)
+      (if (> (length file) 10)
+          (when (string-equal (substring file -10) ".xcodeproj") (setq project-file file))))))
+
+(defun bh-compile ()
+  (interactive)
+  (let ((df (directory-files "."))
+        (has-proj-file nil)
+        )
+    (while (and df (not has-proj-file))
+      (let ((fn (car df)))
+        (if (> (length fn) 10)
+            (if (string-equal (substring fn -10) ".xcodeproj")
+                (setq has-proj-file t)
+              )
+          )
+        )
+      (setq df (cdr df))
+      )
+    (if has-proj-file
+        (compile "xcodebuild -configuration Debug")
+      (compile "make")
+      )
+    )
+  )
+
+(defun bh-choose-header-mode ()
+  (interactive)
+  (if (string-equal (substring (buffer-file-name) -2) ".h")
+      (progn
+        ;; OK, we got a .h file, if a .m file exists we'll assume it's
+        ;; an objective c file. Otherwise, we'll look for a .cpp file.
+        (let ((dot-m-file (concat (substring (buffer-file-name) 0 -1) "m"))
+              (dot-cpp-file (concat (substring (buffer-file-name) 0 -1) "cpp")))
+          (if (file-exists-p dot-m-file)
+              (progn
+                (objc-mode)
+                )
+            (if (file-exists-p dot-cpp-file)
+                (c++-mode)))))))
+(add-hook 'find-file-hook 'bh-choose-header-mode)
